e9f8ee095712676819e239fadd7f3840223d030d
lmeline <loise13@hotmail.fr>
2026-02-19 20:15:13 +0100

    ajout de documentation pour la methode menu

--- api/doc/swaggerOptions.js
+++ api/doc/swaggerOptions.js
@@ -1,16 +1,38 @@
-const path = require('path');
-const swaggerJSDoc = require('swagger-jsdoc');
+const path = require("path");
+const swaggerJSDoc = require("swagger-jsdoc");
 
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'API Restaurant',
-      version: '1.0.0',
-      description: 'Endpoints de l\'API restaurant',
+      title: "API Restaurant",
+      version: "1.0.0",
+      description: "Endpoints de l'API restaurant",
+    },
+    components: {
+      schemas: {
+        Menu: {
+          type: "object",
+          properties: {
+            id: {
+              type: "integer",
+            },
+            name: {
+              type: "string",
+            },
+            price: {
+              type: "number",
+            },
+            category: {
+              type: "string",
+              example: "entree",
+            },
+          },
+        },
+      },
     },
   },
-  apis: [path.join(__dirname, '../http/routes/*.js')],
+  apis: [path.join(__dirname, "../http/routes/*.js")],
 };
 
 module.exports = swaggerJSDoc(options);
--- api/http/routes/menu.js
+++ api/http/routes/menu.js
@@ -2,6 +2,46 @@ const express = require("express");
 const router = express.Router();
 const menuRepository = require("../../repositories/menuRepository");
 
+/**
+ * @swagger
+ * /menu:
+ *   get:
+ *     summary: Get the menu
+ *     tags: [Menu]
+ *     responses:
+ *       200:
+ *         description: The menu
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                     entree:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           id:
+ *                             type: integer
+ *                           name:
+ *                             type: string
+ *                           description:
+ *                             type: string
+ *                           price:
+ *                             type: number
+ *                           category:
+ *                             type: string
+ *                         example: 
+ *                           id: 1
+ *                           name: Salade verte
+ *                           description: Salade verte maison
+ *                           price: 5
+ *                           category: entree
+ *       500:
+ *         description: Internal server error
+ */
+
+
 router.get("/", async (req, res) => {
   try {
     const [menu] = await menuRepository.listMenu();
